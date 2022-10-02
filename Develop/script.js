9777a1835dd660f7e618dead37427cab4f93f672
Juan Aparicio <juan_aparicio98@icloud.com>
2022-10-02 16:07:30 -0500

    added comparisons logic for requirements

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,50 @@ var generateBtn = document.querySelector("#generate");
 
 // Generate password
 function generatePassword() {
+
+  /*
+  // Prompt user for password requirements
   var getPasswordLength = window.prompt("How many characters would you like your password to contain?")
   var includeSpecialChars = window.confirm("Click OK to confirm including special characters.")
   var includeNumericChars = window.confirm("Click Ok to confirm including numeric characters.")
   var includeLowerChars = window.confirm("Click Ok to confirm including lowercase characters.")
   var includeUpperChars = window.confirm("Click Ok to confirm including uppercase characters.")
 
+  /*
+  // Handle errors for password requirements
   var minPasswordLength = window.alert("Password length must be at least 8 characters.")
   var maxPasswordLength = window.alert("Password length cannot exceed 128 characters.")
   var noCharTypes = window.alert("Must select at least one character type.")
+  */
+
+  // Password length requirement
+  var getPasswordLength = window.prompt("How many characters would you like your password to contain?");
+  if (getPasswordLength < 8) {
+    window.alert("Password length must be at least 8 characters.");
+  }
+  if (getPasswordLength > 128) {
+    window.alert("Password length cannot exceed 128 characters.");
+  }
+  if (getPasswordLength == null) {
+    window.alert("Password length must be at least 8 characters.");
+  }
+
+  // Character requirements
+  var includeSpecialChars = window.confirm("Click OK to confirm including special characters.");
+  var includeNumericChars = window.confirm("Click Ok to confirm including numeric characters.");
+  var includeLowerChars = window.confirm("Click Ok to confirm including lowercase characters.");
+  var includeUpperChars = window.confirm("Click Ok to confirm including uppercase characters.");
+  if (includeSpecialChars == false & includeNumericChars == false & includeLowerChars == false & includeUpperChars == false) {
+    window.alert("Must select at least one character type.");
+  }
+
   
+
+
+
+
+
+
   
   // Troubleshooting
   window.alert(
