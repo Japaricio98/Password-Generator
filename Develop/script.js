bfa8a2687ee1daf8d04cd32497e2560734f4b515
Juan Aparicio <juan_aparicio98@icloud.com>
2022-10-02 15:03:48 -0500

    added error handling prompts

--- Develop/script.js
+++ Develop/script.js
@@ -7,14 +7,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Generate password
 function generatePassword() {
-  var getPasswordLength = window.prompt("How many character would you like your password to contain?")
+  var getPasswordLength = window.prompt("How many characters would you like your password to contain?")
   var includeSpecialChars = window.confirm("Click OK to confirm including special characters.")
   var includeNumericChars = window.confirm("Click Ok to confirm including numeric characters.")
   var includeLowerChars = window.confirm("Click Ok to confirm including lowercase characters.")
   var includeUpperChars = window.confirm("Click Ok to confirm including uppercase characters.")
 
-
-
+  var minPasswordLength = window.alert("Password length must be at least 8 characters.")
+  var maxPasswordLength = window.alert("Password length cannot exceed 128 characters.")
+  var noCharTypes = window.alert("Must select at least one character type.")
+  
+  
   // Troubleshooting
   window.alert(
     "getPasswordLength:\t" + getPasswordLength + "\nincludeSpecialChars:\t" + includeSpecialChars + "\nincludeNumericChars:\t" + includeNumericChars + "\nincludeLowerChars:\t" + includeLowerChars + "\nincludeUpperChars:\t" + includeUpperChars
